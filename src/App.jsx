f1a68956608742243e1f56bf3eeab103f2cded02
Litalhag <litalhag@gmail.com>
2023-09-07 10:37:17 +0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const url = 'https://course-api.com/react-tours-project'
 
@@ -6,8 +6,21 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [tours, setTours] = useState([])
 
-  useEffect()
+  const fetchTours = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      console.log(tours)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
   return <h2>Tours Starter</h2>
 }
+
 export default App
-ר
